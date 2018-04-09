91f66d061859dd0a2ac16093f7042a669e50052e
Intera <Intera@RafaelBertoni.bs.br.bsch>
2018-04-09 14:47:47 -0300

    Conectando endpoints

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -14,12 +14,18 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				from("file:pedidos?delay=5s&noop=true").
+				setProperty("pedidoId",xpath("/pedido/id/text()")).
+				setProperty("clienteId",xpath("/pedido/pagamento/email-titular/text()")).
 				split().xpath("/pedido/itens/item").
 				filter().xpath("item/formato[text()='EBOOK']").
+				setProperty("ebookId",xpath("/item/livro/codigo/text()")).
+				log("${id} \n ${body}").
 				marshal().xmljson().
-				log("${id} - ${body}").
-				setHeader(Exchange.FILE_NAME,simple("${file:name.noext}-${header.CamelSplitIndex}.json")).
-				to("file:saida");
+				/*setHeader(Exchange.FILE_NAME,simple("${file:name.noext}-${header.CamelSplitIndex}.json")).
+				to("file:saida");*/
+				//setHeader(Exchange.HTTP_METHOD,constant(HttpMethods.GET)).
+				setHeader(Exchange.HTTP_QUERY,simple("clienteId=${property.clienteId}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
+				to("http4://localhost:8080/webservices/ebook/item");
 			}
 		});
 		
